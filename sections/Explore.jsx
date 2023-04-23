e7e37ffbbc10fc6ef96ebad8c231ba3e8c54efe3
Harshalvk <harshalvkhobragade@gmail.com>
2023-04-23 14:02:58 +0530

    Fix: Tailwind call for width

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -18,7 +18,7 @@ const Explore = () => {
           initial="hidden"
           whileInView="show"
           viewport={{once: false, amount: 0.25}}
-          className={`${styles.innerWidth} mx_auto flex flex-col`}
+          className={`${styles.innerWidth} mx-auto flex flex-col`}
         >
           <TypingText title="| The World" textStyles="text-center"/>
           <TitleText title={<>Choose the world you want <br className="md:block hidden"/> to explore</>} textStyles="text-center"/>
